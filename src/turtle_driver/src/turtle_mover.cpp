084923633aa5e29eed1033131d5fd68f433b3f1d
Chris Okumura <cokumura@umich.edu>
2023-09-09 22:56:33 -0400

    Init

--- src/turtle_driver/src/turtle_mover.cpp
+++ src/turtle_driver/src/turtle_mover.cpp
@@ -2,9 +2,8 @@
 #include "geometry_msgs/Twist.h"
 ros::Publisher vel_publisher;
 
-void drive_circle(double radius, bool isForward);
+void drive_circle(double r, double v, double theta, bool isClockwise);
 
-double speed = 1;
 const double PI = 3.141592653589793238;
 
 int main(int argc, char *argv[])
@@ -14,22 +13,43 @@ int main(int argc, char *argv[])
 
     vel_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
 
-    double radius =0;
 
-    ROS_INFO("\n\n\n********** START TESTING **************");
-    std::cout << "enter radius*: ";
-    std::cin >> radius;
+    double radius=1;
+    double velocity=1;
+    double theta_d=360;
+    bool isClockwise=0;
+    if (argc < 5)
+    {
+        std::cout << "Setting radius to 1" << std::endl;
+        std::cout << "Setting speed to 1" << std::endl;
+        std::cout << "Setting theta to 360" << std::endl;
+        std::cout << "Setting to CCW" << std::endl;
+    }
+    else
+    {
+        char* dummy;
+        std::cout << "Setting radius to " << argv[1] << std::endl;
+        std::cout << "Setting speed to " << argv[2] << std::endl;
+        std::cout << "Setting theta to " << argv[3] << std::endl;
+        std::cout << "Setting to " << (argv[4][0] == '0' ? "CCW" : "CW") << std::endl;
+        radius =strtod(argv[1], &dummy);
+        velocity=strtod(argv[2], &dummy);
+        theta_d = strtod(argv[3], &dummy);
+        isClockwise=atoi(argv[4]);
+    }
+    std::cout << "found: " << radius << std::endl;
 
-    drive_circle(radius, true);
+    drive_circle(radius, velocity, theta_d * PI/180, isClockwise);
 }
 
-void drive_circle(double radius, bool isForward)
+void drive_circle(double r, double v, double theta, bool isClockwise)
 {
     geometry_msgs::Twist vel_msg;
 
-    double expected_length = 2 * PI * radius;
+    double expected_length = theta * r;
+    std::cout << "found: " << theta << std::endl;
 
-    vel_msg.linear.x=speed;
+    vel_msg.linear.x=v;
 
     vel_msg.linear.y=0;
     vel_msg.linear.z=0;
@@ -40,24 +60,30 @@ void drive_circle(double radius, bool isForward)
     //   t = 360 / ang_vel
     //     d_total = speed * t = speed * 360 / ang_vel
     //     2 * pi * radius = speed * 360 / ang_vel
-    //        ang_vel = speed * 360 / (2 * pi * radius)     
+    //        ang_vel = speed * 360 / (2 * pi * radius)  
+
+    // 2pi = ang_vel * t   
+    // 2pi * r = v * t
+    // ang_vel = v/r
 
     // double compute_ang_vel = speed * (2*PI) / (2 * PI * radius);
-    double compute_ang_vel = speed / radius;
+    double compute_ang_vel = v / r * isClockwise ? 1 : -1;
 
     vel_msg.angular.z=compute_ang_vel;
     
     double t0 = ros::Time::now().toSec();
     double current_angle=0;
-    ros::Rate loop_rate(10);
+    double curr_dist =0;
+    ros::Rate loop_rate(100);
     do 
     {
         vel_publisher.publish(vel_msg);
         double t1 = ros::Time::now().toSec();
         current_angle = compute_ang_vel * (t1-t0);
+        curr_dist = v * (t1 - t0);
         ros::spinOnce();
         loop_rate.sleep();
-    } while (current_angle < 2 * PI);
+    } while (abs(curr_dist) < expected_length);
 
     geometry_msgs::Twist stop_msg;
     stop_msg.linear.x = 0;
@@ -66,5 +92,5 @@ void drive_circle(double radius, bool isForward)
     stop_msg.angular.x = 0;
     stop_msg.angular.y = 0;
     stop_msg.angular.z = 0;
-
+    vel_publisher.publish(stop_msg);
 }
