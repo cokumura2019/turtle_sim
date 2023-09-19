c88af3291c9a668d4b8a5a909bd47258fc5c560e
Chris Okumura <cokumura@umich.edu>
2023-09-18 22:25:50 -0400

    Removing big comment block

--- turtle_driver/src/turtle_mover.cpp
+++ turtle_driver/src/turtle_mover.cpp
@@ -42,70 +42,6 @@ std::string pose_subscriber_topic_name;
 std::string vel_publish_topic_name;
 std::string tele_publish_topic_name;
 
-/*
-int main(int argc, char *argv[])
-{
-    ros::init(argc, argv, "turtle_mover");
-    ros::NodeHandle n;
-
-    pos_subscriber = n.subscribe("/turtle1/pose", 1000, posecallback);
-    vel_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
-    
-    int mode;
-    ROS_INFO("\n\n\n********** START TESTING **************");
-    std::cout << "Enter mode: ";
-    std::cin >> mode;
-
-    stop_msg.linear.x = 0;
-    stop_msg.linear.y = 0;
-    stop_msg.linear.z = 0;
-    stop_msg.angular.x = 0;
-    stop_msg.angular.y = 0;
-    stop_msg.angular.z = 0;
-    init_position();
-
-  //  drive_circle(radius, true);
-    if (mode == 0)
-    {         
-        double radius = 1;
-        try
-        {
-            radius = atoi(argv[1]);
-        }
-        catch(const std::exception& e)
-        {
-        }
-        drive_circle(radius, true);   
-    }
-    else if (mode == 1)
-    {            
-        double sideLength = 1;
-        try
-        {
-            sideLength = atoi(argv[1]);
-        }
-        catch(const std::exception& e)
-        {
-        }
-        drive_square(sideLength);   
-    }
-    else
-    {
-        int numPoints = (argc - 1) / 2;
-        vector2 points[numPoints];
-        char* dummyptr;
-        for (int i = 0 ; i < numPoints ;i++)
-        {
-            
-            points[i] = {strtof(argv[i * 2 + 1], &dummyptr), strtof(argv[i*2 +2], &dummyptr)};
-        }
-        std::cout << points[1].x << std::endl;
-        follow_points(points, numPoints);
-    }
-
-    return 0;
-}
-*/
 
 int main(int argc, char *argv[])
 {
@@ -213,7 +149,7 @@ bool drive_circle(turtle_driver::circle::Request  &req, turtle_driver::circle::R
 
     // ang_vel * t = 360
     //   t = 360 / ang_vel
-    //     d_total = speed * t = speed * 360 / ang_vel
+    //     d_total = speed * t = speed * 360 / ang_velS
     //     2 * pi * radius = speed * 360 / ang_vel
     //        ang_vel = speed * 360 / (2 * pi * radius)     
 
