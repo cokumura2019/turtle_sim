fdbc7b3082731fc3dd668e48245ac27d27b9f4a3
Chris Okumura <cokumura@umich.edu>
2023-09-18 22:20:42 -0400

    OOP version of the python script

--- turtle_driver_ui/scripts/turtle_controller.py
+++ turtle_driver_ui/scripts/turtle_controller.py
@@ -5,73 +5,113 @@ from __future__ import print_function
 import sys
 import rospy
 from turtle_driver.srv import *
+from abc import ABC, abstractmethod
 
 
-def drive_circle(radius):
-    rospy.wait_for_service('circle')
-    try:
-        drive = rospy.ServiceProxy('circle', circle)
-        resp1 = drive(radius)
-        return resp1.response
-    except rospy.ServiceException as e:
-        return 0
+class drive_command_parser(ABC):
+    @abstractmethod
+    def on_message_received(self, str_mode, params):
+        pass
 
-def drive_square(length):
-    rospy.wait_for_service('square')
-    try:
-        drive = rospy.ServiceProxy('square', square)
-        resp1 = drive(length)
-        return resp1.response
-    except rospy.ServiceException as e:
-        return 0
+class circle_driver(drive_command_parser):
 
-def drive_custom(x_points, y_points):
-    rospy.wait_for_service('custom')
-    try:
-        drive = rospy.ServiceProxy('custom', custom)
-        resp1 = drive(x_points, y_points)
-        return resp1.response
-    except rospy.ServiceException as e:
-        return 0
+    def on_message_received(self, str_mode, params):
+        response = 0
+        if (str_mode == 'circle'):
+            radius = float(params[0])
 
-if __name__ == "__main__":
-    rospy.init_node("turtle_controller_ui_cli")
+            rospy.wait_for_service('circle')
+            try:
+                drive = rospy.ServiceProxy('circle', circle)
+                resp1 = drive(radius)
+                response = resp1.response
+            except rospy.ServiceException as e:
+                pass
+
+            if (response == 1):
+                print("Circle maneuever succeeded")
+            else:
+                print("Circle maneuever has failed")
+
+        return response 
+
+class square_driver(drive_command_parser):
     
-    print("Requesting drive\n")
-    while not rospy.is_shutdown():
-        cmd = input("Please insert command! Type 'h' for help.\n")
+    def on_message_received(self, str_mode, params):
+        response = 0
+        if (str_mode == 'square'):
+            length = float(params[0])
 
-        if (cmd == 'h'):
+            rospy.wait_for_service('square')
+            try:
+                drive = rospy.ServiceProxy('square', square)
+                resp1 = drive(length)
+                response = resp1.response
+            except rospy.ServiceException as e:
+                pass
+
+            if (response == 1):
+                print("Square maneuever succeeded")
+            else:
+                print("Square maneuever has failed")
+
+        return response 
+
+class custom_driver(drive_command_parser):
+
+    def on_message_received(self, str_mode, params):
+        response = 0
+        if (str_mode == 'custom'):
+            x_pts = []
+            y_pts =[]
+            for i in range(0, len(params)):
+                split = params[i].split(',')
+                x_pts.append(float(split[0]))
+                y_pts.append(float(split[1]))
+
+            rospy.wait_for_service('custom')
+            try:
+                drive = rospy.ServiceProxy('custom', custom)
+                resp1 = drive(x_pts, y_pts)
+                response = resp1.response
+            except rospy.ServiceException as e:
+                pass
+
+            if (response == 1):
+                print("Custom maneuever succeeded")
+            else:
+                print("Custom maneuever has failed")
+
+        return response 
+
+class help_printer(drive_command_parser):
+    def on_message_received(self, str_mode, params):
+        if (str_mode == 'h'):
             print("""
             circle <radius> => drive in a circle of specified radius
             square <side_length> => drive in a square of specified side length
             custom <p1> <p2> <p3>... => follow these points sequentially where p1 in the form <x,y>         
             """)
-        else:
-            args = cmd.split()
-            if (args[0] == 'circle'):
-                if drive_circle(float(args[1])):
-                    print("Circle maneuever succeeded")
-                else:
-                    print("Circle maneuever has failed")
-            elif (args[0] == 'square'):
-                if drive_square(float(args[1])):
-                    print("Square maneuever succeeded")
-                else:
-                    print("Square maneuever has failed")
-            elif (args[0] == 'custom'):
-                x_pts = []
-                y_pts =[]
-                for i in range(1, len(args)):
-                    split = args[i].split(',')
-                    x_pts.append(float(split[0]))
-                    y_pts.append(float(split[1]))
-                if drive_custom(x_pts, y_pts):
-                    print("Custom maneuever succeeded")
-                else:
-                    print("Custom maneuever has failed")
-            else:
-                print("Couldn't understand your command. Please ensure it is a supported command. See help for more")
+            return 1
+        return 0
 
+
+
+if __name__ == "__main__":
+    rospy.init_node("turtle_controller_ui_cli")
+
+    command_parsers = [circle_driver(), square_driver(), custom_driver(), help_printer()]
+
+    print("Requesting drive\n")
+    while not rospy.is_shutdown():
+        cmd = input("Please insert command! Type 'h' for help.\n")
+        args = cmd.split()
+
+        success = 0
+        for parser in command_parsers: 
+            success |= parser.on_message_received(args[0], args[1:] if len(args) >= 2 else None)
+
+        if (not success):
+                print("Couldn't understand your command. Please ensure it is a supported command. See help for more")    
         print("\n")
 
